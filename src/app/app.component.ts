d04068e85a9fee426070b4ea67d15224a0c59939
Geoff Morris <geoffm@gmail.com>
2018-12-29 11:49:14 -0800

    loading tfjs model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'robofoodie';
+  model: tf.Model;
+  prediction: any;
+
+  ngOnInit() {
+    this.loadModel();
+  }
+
+  async loadModel() {
+    console.log("Loading model...");
+    this.model = await tf.loadModel('/assets/model.json');
+    console.log("Model loaded");
+  }
+
+  async predict() {
+    const pred = await tf.tidy(() => {
+        console.log("TODO: make prediction");
+    });
+  }
 }
